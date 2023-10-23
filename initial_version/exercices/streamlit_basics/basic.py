d665142fa9549a40b1fb96ab87f6d4d65d88c56e
Jeromik <135344819+Jeromik@users.noreply.github.com>
2023-10-23 10:59:45 +0200

    Update basic.py

--- initial_version/exercices/streamlit_basics/basic.py
+++ initial_version/exercices/streamlit_basics/basic.py
@@ -57,12 +57,21 @@ def display_media():
     # st.set_page_config(layout="wide")
 
     width = st.sidebar.slider(label="Width", min_value=0, max_value=100, value=DEFAULT_WIDTH, format="%d%%")
+    # width = st.slider(label="Width", min_value=0, max_value=100, value=DEFAULT_WIDTH, format="%d%%")
 
     width = max(width, 0.01)
     side = max((100 - width) / 2, 0.01)
 
     _, container, _ = st.columns([side, width, side])
     container.video(data=VIDEO_DATA)
+
+ def layout():
+    st.header("Totu sur le layout")
+
+    #Sidebar
+    st.sidebar.title("Titre de barre navigation")  
+    st.sidebar.write("projet des accidents")
+    st.sidebar.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
 #######################################################
 #                       START
